e7cb8b2c844986044bef2c3ad73af2d1ec157fb1
Galen Sho Sato <galen.sato@gmail.com>
2023-08-07 22:54:12 -0400

    Created basic Header and NavBar components; added check_session fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react'
 import './App.css';
+import Header from './Components/Header';
+import NavBar from './Components/NavBar';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({})
+
+  useEffect(() => {
+    fetch('/api/check_session')
+    .then(resp => {
+      if (resp.ok) {
+        resp.json()
+        .then((user) => setCurrentUser(user))
+      }
+    })
+  }, [])
+
+
   return (
     <div>
+      <Header />
+      <br></br>
+      <NavBar currentUser = {currentUser} setCurrentUser = {setCurrentUser} />
     </div>
   );
 }
---
+++ client/src/Components/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header() {
+    return (
+        <>
+        <h1>Gambling Header</h1>
+        </>
+    )
+}
+
+export default Header
