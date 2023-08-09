1cce9bce46360a4149b27923db81fed06f749846
Galen Sho Sato <galen.sato@gmail.com>
2023-08-08 23:15:13 -0400

    Added to NavBar Component

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,8 +1,17 @@
-
+import React from 'react'
+import { Container, Nav, Navbar} from "react-bootstrap"
+import { useNavigate } from 'react-router-dom'
 
 function NavBar() {
     return (
         <>
+        <Navbar>
+            <Container>
+                <Nav>
+                    
+                </Nav>
+            </Container>
+        </Navbar>
         </>
     )
 }
