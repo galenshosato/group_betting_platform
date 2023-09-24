692d9f5f2c9ef701402bf4d5777c2da01df3f49f
Galen Sho Sato <galen.sato@gmail.com>
2023-09-24 19:43:09 -0400

    adjusted more imports

--- server/seed.py
+++ server/seed.py
@@ -1,18 +1,18 @@
-from app import app
-from extensions import db
-from models import User
-import bcrypt
+# from app import app
+# from extensions import db
+# from models import User
+# import bcrypt
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        print("Hashing Passwords...")
-        users = User.query.all()
+# if __name__ == "__main__":
+#     with app.app_context():
+#         print("Hashing Passwords...")
+#         users = User.query.all()
 
-        for user in users:
-            password = user.password
-            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-            user.password = hashed_password
-            db.session.add(user)
+#         for user in users:
+#             password = user.password
+#             hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+#             user.password = hashed_password
+#             db.session.add(user)
 
-        db.session.commit()
+#         db.session.commit()
