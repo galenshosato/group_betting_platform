e4b1c32cafdb6312e333d519c71f3f8f365866d6
Galen Sho Sato <galen.sato@gmail.com>
2023-09-24 22:55:25 -0400

    re-deployed

---
+++ server/configure.py
@@ -0,0 +1,3 @@
+plain_text_password = "ct6dk947dwcwdcln"
+
+DATABASE_URI = f"mysql+mysqlconnector://yyb4c71qebmurlz2:{plain_text_password}@l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nxw0e924tjfniumf"
